cc0eb9e96110b130b467841c494c8ebeff0f5691
Aditya Bhattacharjee <adityab76@gmail.com>
2023-06-30 16:24:45 +0530

    frontend folder added

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+ import React from 'react'
+import Navbar from './components/NavBar'
+import AllRoutes from './Routing/AllRoutes'
+
+
+
+const App = () => {
+  return (
+    <div className='container'>
+       <Navbar />  
+      
+      <AllRoutes />
+    </div>
+  )
+}
+
+export default App
