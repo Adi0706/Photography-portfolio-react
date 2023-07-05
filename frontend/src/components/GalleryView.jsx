501e78ac5a307882585e1e9a9ff6a89b41e3fd9a
Aditya Bhattacharjee <adityab76@gmail.com>
2023-07-05 13:29:20 +0530

    final changes

--- frontend/src/components/GalleryView.jsx
+++ frontend/src/components/GalleryView.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import Lightbox from "yet-another-react-lightbox";
-import "yet-another-react-lightbox/styles.css";
+import { Modal } from "react-responsive-modal";
+import "react-responsive-modal/styles.css";
 import PhotoONE from '../images/photo1.png';
 import PhotoTWO from '../images/photo2.png';
 import PhotoTHREE from '../images/photo3.png';
@@ -9,6 +9,8 @@ import PhotoFIVE from '../images/photo5.png';
 import PhotoSIX from '../images/photo6.png';
 import PhotoSEVEN from '../images/photo7.png';
 import PhotoEIGHT from '../images/photo8.png';
+import PhotoNINE from '../images/photo9.png';
+import { colors } from "@mui/material";
 
 const GalView = () => {
   const [open, setOpen] = useState(false);
@@ -19,36 +21,36 @@ const GalView = () => {
     setOpen(true);
   };
 
-  const slides = [
-    { src: PhotoONE, alt: "image 1" },
-    { src: PhotoTWO, alt: "image 2" },
-    { src: PhotoTHREE, alt: "image 3" },
-    { src: PhotoFOUR, alt: "image 4" },
-    { src: PhotoFIVE, alt: "image 5" },
-    { src: PhotoSIX, alt: "image 6" },
-    { src: PhotoSEVEN, alt: "image 7" },
-    { src: PhotoEIGHT, alt: "image 8" },
-  ];
+  const closeLightbox = () => {
+    
+    setOpen(false);
+  };
 
-  const imageElements = slides.map((slide, index) => (
-    <img
-      key={index}
-      src={slide.src}
-      alt={slide.alt}
-      onClick={() => openLightbox(index)}
-    />
-  ));
+  const photos = [
+    { src: PhotoONE, alt: "image 1", width: 1, height: 1 },
+    { src: PhotoTWO, alt: "image 2", width: 1, height: 1 },
+    { src: PhotoTHREE, alt: "image 3", width: 1, height: 1 },
+    { src: PhotoFOUR, alt: "image 4", width: 1, height: 1 },
+    { src: PhotoFIVE, alt: "image 5", width: 1, height: 1 },
+    { src: PhotoSIX, alt: "image 6", width: 1, height: 1 },
+    { src: PhotoSEVEN, alt: "image 7", width: 1, height: 1 },
+    { src: PhotoEIGHT, alt: "image 8", width: 1, height: 1 }, 
+    { src: PhotoNINE, alt: "image 8", width: 1, height: 1 }
+  ];
 
   return (
     <div className="Galcontainer">
-      <div className="gallery">{imageElements}</div>
-
-      <Lightbox
-        open={open}
-        currentIndex={currentIndex}
-        onClose={() => setOpen(false)}
-        slides={slides.map((slide) => ({ ...slide }))}
-      />
+      {photos.map((photo, index) => (
+        <img
+          key={index}
+          src={photo.src}
+          alt={photo.alt}
+          onClick={() => openLightbox(index)}
+        />
+      ))}
+      <Modal open={open} onClose={closeLightbox} center classNames={{ closeButton: 'custom-close-button' }}>
+        <img src={photos[currentIndex].src} alt={photos[currentIndex].alt} />
+      </Modal>
     </div>
   );
 };
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,31 +1,68 @@
-import {FaInstagram,FaLinkedinIn,FaGithub} from 'react-icons/fa'
-import { Link } from 'react-router-dom' ; 
+import { FaInstagram, FaLinkedinIn, FaGithub } from 'react-icons/fa';
+import { Link, NavLink } from 'react-router-dom';
+import { useState } from 'react';
 
+const Navbar = () => {
+  const [activeNavItem, setActiveNavItem] = useState('');
 
-const Navbar=()=>{
-    return(
-        <>
-        
-        <nav className="nav-bar">
-            <ul className="nav-items">
-                <li className='function'><Link to="/" className='link'>HOME</Link></li>
-                <li className='function'><Link to="/About" className='link'>ABOUT</Link></li>
-                <li className='function'><Link to="/Gallery" className='link'>GALLERY</Link></li>
-                <li className='function'><Link to="/Contact" className='link'>CONTACT</Link></li>
-                </ul>
-                <ul className='icon-tray'>
-                 <li className='icon'><a href="https://www.instagram.com/thephotographypage8/"><FaInstagram /></a></li>
-                <li className='icon'><a href="https://www.linkedin.com/in/aditya-bhattacharjee-288462215/"><FaLinkedinIn/></a></li>
-                <li className='icon'><a href='https://github.com/Adi0706'><FaGithub /></a></li> 
-                
-           
-            </ul>
-            
-        </nav>
-        
-        
-        </>
+  const handleNavItemClick = (itemName) => {
+    setActiveNavItem(itemName);
+  };
 
-    );
-    }
-    export default Navbar ; 
+  return (
+    <>
+      <nav className="nav-bar">
+        <ul className="nav-items">
+          <li className='function'>
+            <NavLink
+              exact
+              to="/"
+              className='link'
+              activeClassName='active'
+              onClick={() => handleNavItemClick('HOME')}
+            >
+              HOME
+            </NavLink>
+          </li>
+          <li className='function'>
+            <NavLink
+              to="/About"
+              className='link'
+              activeClassName='active'
+              onClick={() => handleNavItemClick('ABOUT')}
+            >
+              ABOUT
+            </NavLink>
+          </li>
+          <li className='function'>
+            <NavLink
+              to="/Gallery"
+              className='link'
+              activeClassName='active'
+              onClick={() => handleNavItemClick('GALLERY')}
+            >
+              GALLERY
+            </NavLink>
+          </li>
+          <li className='function'>
+            <NavLink
+              to="/Contact"
+              className='link'
+              activeClassName='active'
+              onClick={() => handleNavItemClick('CONTACT')}
+            >
+              CONTACT
+            </NavLink>
+          </li>
+        </ul>
+        <ul className='icon-tray'>
+          <li className='icon'><a href="https://www.instagram.com/thephotographypage8/"><FaInstagram /></a></li>
+          <li className='icon'><a href="https://www.linkedin.com/in/aditya-bhattacharjee-288462215/"><FaLinkedinIn /></a></li>
+          <li className='icon'><a href='https://github.com/Adi0706'><FaGithub /></a></li>
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
